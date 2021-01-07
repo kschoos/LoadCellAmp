50c0f5b9b1f02c667d1924da0d661c1669810458
Kai Schoos <kschoos@outlook.com>
2021-01-07 12:47:07 +0100

    Added data pin config and isr code

--- src/arch/espidf/components/loadCellAmp/loadCellAmp.cpp
+++ src/arch/espidf/components/loadCellAmp/loadCellAmp.cpp
@@ -46,10 +46,11 @@ static void IRAM_ATTR dataISR(void* params){
 
 // Hardware Setup ==============================================================================================
 // =============================================================================================================
-
 void LoadCellAmp::setupGPIO(){
   gpio_config_t io_conf;
-  uint64_t output_mask = 1 << this->sp_clk_pin;
+
+  // SP_CLK GPIO
+  uint64_t output_mask = 1 << this->sp_clk_pin; 
 
   io_conf.intr_type= GPIO_INTR_DISABLE;
   io_conf.mode = GPIO_MODE_OUTPUT;
@@ -58,6 +59,23 @@ void LoadCellAmp::setupGPIO(){
   io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
 
   gpio_config(&io_conf);
+
+  // Data GPIO
+  uint64_t input_mask = 1 << this->dout_pin; 
+  
+  io_conf.intr_type = GPIO_INTR_NEGEDGE;
+  io_conf.mode = GPIO_MODE_INPUT;
+  io_conf.pin_bit_mask = input_mask;
+  // TODO: Revise. For now, we leave pulling up and down to the amp
+  io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+
+  gpio_config(&io_conf);
+
+  // We want to disable the interrupt for this after it is triggered for as long as we are reading data.
+  // After that we will enable it again. (ESP_INTR_FLAG_INTRDISABLED)
+  gpio_install_isr_service( ESP_INTR_FLAG_INTRDISABLED | ESP_INTR_FLAG_EDGE | ESP_INTR_FLAG_LEVEL1 ) ;
+  gpio_isr_handler_add(this->dout_pin, dataISR, 0);
 } 
 
 void LoadCellAmp::setupClkTimer(){
